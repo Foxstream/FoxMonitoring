a9bebf1ecbd9df89bfe1c0d1434a65c408cc0128
Ionel POP <i.pop@foxstream.fr>
2018-02-22 16:52:15 +0100

    Scripts de maj ok

---
+++ src/setup/install.cmd
@@ -0,0 +1,10 @@
+#echo off
+SET INSTALL_FOLDER=C:\Foxstream\
+SET MONITORING_FOLDER=%INSTALL_FOLDER%\FoxMonitoring
+
+mkdir %INSTALL_FOLDER%
+cd %INSTALL_FOLDER%
+
+git clone https://github.com/Foxstream/FoxMonitoring.git
+
+Schtasks /create /F /SC daily /TN UpdateFoxMonitoring /ST 12:00:00 /TR %MONITORING_FOLDER%\setup\update.cmd
